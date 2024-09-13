f39d3e80014a149e683160134386937285a65453
ale24dev <ale.24dev@gmail.com>
2024-09-13 18:54:21 -0400

    add: game screen implemented

---
+++ lib/src/features/home/widgets/game_section.dart
@@ -0,0 +1,104 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gutter/flutter_gutter.dart';
+import 'package:my_app/src/core/ui/typography.dart';
+import 'package:my_app/src/features/home/widgets/otp_fields.dart';
+import 'package:my_app/src/features/home/widgets/play_number_card.dart';
+import 'package:sized_context/sized_context.dart';
+
+class GameSection extends StatelessWidget {
+  const GameSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('Time left: ', style: AppTextStyle().body),
+            const Text('30'),
+          ],
+        ),
+        const _PlayList(),
+        const Gutter(),
+        const _SendNumberSection(),
+      ],
+    );
+  }
+}
+
+class _PlayList extends StatelessWidget {
+  const _PlayList();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.heightPx * .6,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Text(
+            'Previous attempts'.toUpperCase(),
+            style: AppTextStyle().body.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const GutterSmall(),
+          ...List.generate(4, (index) {
+            return const PlayNumberCard();
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class _SendNumberSection extends StatefulWidget {
+  const _SendNumberSection();
+
+  @override
+  __SendNumberSectionState createState() => __SendNumberSectionState();
+}
+
+class __SendNumberSectionState extends State<_SendNumberSection> {
+  String otpValue = '';
+
+  void _onCompleted(String otp) {
+    setState(() {
+      otpValue = otp;
+    });
+  }
+
+  void _onChanged(String otp) {
+    setState(() {
+      otpValue = otp;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ///This is to center the OTPFields
+        const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.send, color: Colors.transparent),
+        ),
+        OTPFields(
+          allowRepetitions: false,
+          onCompleted: _onCompleted,
+          onChanged: _onChanged,
+        ),
+        IconButton(
+          onPressed: () {
+            log('OTP enviado: $otpValue');
+          },
+          icon: Icon(Icons.send, color: Theme.of(context).colorScheme.primary),
+        ),
+      ],
+    );
+  }
+}
--- lib/src/features/home/widgets/versus_section.dart
+++ lib/src/features/home/widgets/versus_section.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
 import 'package:my_app/src/core/ui/colors.dart';
 import 'package:my_app/src/core/ui/device.dart';
 import 'package:my_app/src/core/ui/typography.dart';
+import 'package:my_app/src/features/home/cubit/game_cubit.dart';
 
 class VersusSection extends StatelessWidget {
   const VersusSection({super.key});
@@ -10,7 +12,8 @@ class VersusSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: context.responsiveContentPadding.copyWith(top: 50),
+      padding: context.responsiveContentPadding
+          .copyWith(top: 50, left: 30, right: 30),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
@@ -30,7 +33,9 @@ class VersusSection extends StatelessWidget {
               Text(
                 'Username',
                 style: AppTextStyle().body.copyWith(
-                    fontWeight: FontWeight.w600, color: AppColor.titleText,),
+                      fontWeight: FontWeight.w600,
+                      color: AppColor.titleText,
+                    ),
               ),
               Text(
                 'Rank: 1',
@@ -40,8 +45,42 @@ class VersusSection extends StatelessWidget {
               ),
             ],
           ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              _exitGame(context);
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
         ],
       ),
+    );
+  }
+
+  Future<dynamic> _exitGame(BuildContext context) {
+    return showAdaptiveDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog.adaptive(
+          title: const Text('Are you sure?'),
+          content: const Text('Do you want to exit the game?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<GameCubit>().refresh();
+                Navigator.of(context).pop();
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }

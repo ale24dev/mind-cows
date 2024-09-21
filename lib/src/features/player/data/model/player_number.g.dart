17954911d34ac2708b4afbc207802ed7d64b02e8
ale24dev <ale.24dev@gmail.com>
2024-09-21 03:48:52 -0400

    add: add new properties on player number and use equatable

--- lib/src/features/player/data/model/player_number.g.dart
+++ lib/src/features/player/data/model/player_number.g.dart
@@ -9,6 +9,8 @@ part of 'player_number.dart';
 PlayerNumber _$PlayerNumberFromJson(Map<String, dynamic> json) => PlayerNumber(
       id: (json['id'] as num).toInt(),
       player: Player.fromJson(json['player'] as Map<String, dynamic>),
+      isTurn: json['is_turn'] as bool,
+      timeLeft: (json['time_left'] as num).toInt(),
       number: (json['number'] as List<dynamic>?)
           ?.map((e) => (e as num).toInt())
           .toList(),
@@ -18,5 +20,7 @@ Map<String, dynamic> _$PlayerNumberToJson(PlayerNumber instance) =>
     <String, dynamic>{
       'id': instance.id,
       'player': instance.player.toJson(),
+      'is_turn': instance.isTurn,
+      'time_left': instance.timeLeft,
       'number': instance.number,
     };

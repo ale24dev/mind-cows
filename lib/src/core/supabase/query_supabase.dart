6f0f82dfc53df23825d9c296e156cdbc9a66caa0
ale24dev <ale.24dev@gmail.com>
2024-09-21 03:50:14 -0400

    add: new properties to queryBase

--- lib/src/core/supabase/query_supabase.dart
+++ lib/src/core/supabase/query_supabase.dart
@@ -2,7 +2,8 @@
 abstract class QuerySupabase {
   static String get player => 'id, username, avatar_url';
 
-  static String get playerNumber => 'id, player($player), number';
+  static String get playerNumber =>
+      'id, player($player), number, is_turn, time_left';
 
   static String get game => '''
     id, status!inner($gameStatus), player_number1!inner($playerNumber), player_number2!inner($playerNumber), winner($player)

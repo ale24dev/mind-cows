d29802e6e51ecfbbc37017c3a893fb6c951dcf1b
ale24dev <ale.24dev@gmail.com>
2024-09-22 14:58:06 -0400

    add: ranking utils

---
+++ lib/src/features/ranking/uitls/ranking_utils.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/resources/resources.dart';
+import 'package:my_app/src/features/ranking/data/model/ranking.dart';
+
+enum PodiumPosition { first, second, third }
+
+abstract class RankingUtils {
+  static List<Ranking> podium(List<Ranking> ranking) {
+    // If there are less than 3 rankings, return the original list
+    if (ranking.length < 3) return ranking;
+
+    // Get the first three elements
+    final podium = ranking.sublist(0, 3);
+
+    // Rearrange the list to show 2nd, 1st, and then 3rd
+    return [podium[1], podium[0], podium[2]];
+  }
+
+  static String getImageRankingByPos(PodiumPosition podiumPosition) {
+    return switch (podiumPosition) {
+      PodiumPosition.first => AppImages.firstPlace,
+      PodiumPosition.second => AppImages.secondPlace,
+      _ => AppImages.thirdPlace
+    };
+  }
+
+  static double getRotationByPos(PodiumPosition podiumPosition) {
+    return switch (podiumPosition) {
+      PodiumPosition.first => 0,
+      PodiumPosition.second => -0.1,
+      _ => 0.1,
+    };
+  }
+}

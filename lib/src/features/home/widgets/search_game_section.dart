923e0938d1f7639ec3c1eb9ed60c1f27fed5f4ef
ale24dev <ale.24dev@gmail.com>
2024-10-03 03:49:00 -0400

    feat/ logout user confirmation

--- lib/src/features/home/widgets/search_game_section.dart
+++ lib/src/features/home/widgets/search_game_section.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: inference_failure_on_function_invocation
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
@@ -5,7 +7,6 @@ import 'package:go_router/go_router.dart';
 import 'package:my_app/src/core/ui/theme.dart';
 import 'package:my_app/src/core/ui/typography.dart';
 import 'package:my_app/src/features/auth/cubit/auth_cubit.dart';
-import 'package:my_app/src/features/auth/views/auth_screen.dart';
 import 'package:my_app/src/features/game/cubit/game_cubit.dart';
 import 'package:my_app/src/router/router.dart';
 import 'package:sized_context/sized_context.dart';
@@ -33,64 +34,99 @@ class _SearchGameSectionState extends State<SearchGameSection> {
               (_) => context.goNamed(AppRoute.game.name),
             );
           }
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: GestureDetector(
-                  onTap: () => context.read<AuthCubit>().logout(),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.logout),
-                      const GutterTiny(),
-                      Text(
-                        'Exit',
-                        style: AppTextStyle().body.copyWith(
-                              color: Theme.of(context).colorScheme.onSurface,
+          return SizedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _showLogoutConfirmationDialog(context),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.logout),
+                        const GutterTiny(),
+                        Text(
+                          'Exit',
+                          style: AppTextStyle().body.copyWith(
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      context.goNamed(AppRoute.searchGame.name);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: AppTheme.defaultShadow,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.play_arrow, color: Colors.white),
+                            const GutterTiny(),
+                            Text(
+                              'Play',
+                              style: AppTextStyle()
+                                  .textButton
+                                  .copyWith(color: Colors.white),
                             ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    context.goNamed(AppRoute.searchGame.name);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: AppTheme.defaultShadow,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 10,
-                      ),
-                      child: Row(
-                        children: [
-                          const Icon(Icons.play_arrow, color: Colors.white),
-                          const GutterTiny(),
-                          Text(
-                            'Play',
-                            style: AppTextStyle()
-                                .textButton
-                                .copyWith(color: Colors.white),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const Expanded(child: SizedBox.shrink()),
-            ],
+                const Expanded(child: SizedBox.shrink()),
+              ],
+            ),
           );
         },
       ),
+    );
+  }
+
+  void _showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Logout'),
+          content: const Text('Are you sure you want to logout?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Cancel',
+                style: AppTextStyle().textButton.copyWith(color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                context.read<AuthCubit>().logout();
+              },
+              child: Text(
+                'Logout',
+                style: AppTextStyle()
+                    .textButton
+                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
